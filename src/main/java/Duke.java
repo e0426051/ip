39d40140d507ebd94b4a1fef2b671ce77e98b90e
e0426051 <e0426051@u.nus.edu>
2020-08-29 05:35:04 -0700

    Level-2 : Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,14 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
     public static void main(String[] args) {
         String input;
         int byeCheck = 1;
+        int listToggle = 1;
+        int listCount = 0;
+        int i = 0;
+        String[] list = new String[100];
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -16,10 +21,23 @@ public class Duke {
         while(byeCheck != 0) {
             input = scan.nextLine();
             byeCheck = input.compareToIgnoreCase("bye");
+            listToggle = input.compareToIgnoreCase("list");
             if (byeCheck == 0) {
                 System.out.println("Bye. Hope to see you again soon!");
-            } else {
-                System.out.println(input);
+            }
+            else if (listToggle == 0) {
+                for (i = 0; i < list.length; i++) {
+                    if (list[i] != null) {
+                        System.out.println(i + 1 + ": " + list[i]);
+                    }
+                }
+                //continue;
+            }
+            else {
+                //put into list
+                list[listCount] = input;
+                listCount++;
+                System.out.println("Added: " + input);
             }
         }
     }
