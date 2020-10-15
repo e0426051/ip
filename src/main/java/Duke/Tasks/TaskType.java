939752af02b87b767e4939d2ab0ce6efaa4b7a58
e0426051 <e0426051@u.nus.edu>
2020-10-15 08:41:00 -0700

    Fix duplicate directory issue attempt

--- src/main/java/Duke/Tasks/TaskType.java
+++
@@ -1,5 +0,0 @@
-package duke.tasks;
-
-public enum TaskType {
-    DEADLINE, EVENT, TODO, TRADITIONAL_TASK
-}
--- src/main/java/duke/parser/CommandType.java
+++
@@ -1,8 +0,0 @@
-package duke.parser;
-
-/**
- * This enum ensures that all commands are categorized into an appropriate command type.
- */
-public enum CommandType {
-    LIST, DELETE, TODO, DEADLINE, TRADITIONAL_TASK, EVENT, FIND, DONE, BYE
-}
